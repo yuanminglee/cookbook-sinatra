6eea4a442a88980051e53b301b7483356b5e7dd5
YuanMing Lee <yuanming.lee91@gmail.com>
2021-05-28 14:18:42 +0800

    Commit

--- app.rb
+++ app.rb
@@ -2,11 +2,20 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  erb :team
 end
